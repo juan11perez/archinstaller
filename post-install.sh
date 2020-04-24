854795eda2300efd203951e8f3f1a5ff12593bce
juan11perez <juan11perez@yahoo.com>
2020-04-24 12:19:10 +0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,20 +3,28 @@
 # This is Configuration script of Krushn's Arch Linux Installation Package.
 # Visit krushndayshmookh.github.io/krushn-arch for instructions.
 
-echo "Krushn's Arch Configurator"
+echo "Arch Configurator"
+
+# Install swap file
+fallocate -l 3G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
 
 # Set date time
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 hwclock --systohc
 
 # Set locale to en_US.UTF-8 UTF-8
 sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+sed -i '/en_GB.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
 
 # Set hostname
-echo "dayshmookh" >> /etc/hostname
-echo "127.0.1.1 dayshmookh.localdomain  dayshmookh" >> /etc/hosts
+echo "archlinux" >> /etc/hostname
+echo "127.0.1.1 dayshmookh.localdomain archlinux" >> /etc/hosts
 
 # Generate initramfs
 mkinitcpio -P
@@ -25,19 +33,25 @@ mkinitcpio -P
 passwd
 
 # Install bootloader
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
+# grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
+# grub-mkconfig -o /boot/grub/grub.cfg
 
 # Create new user
-useradd -m -G wheel,power,iput,storage,uucp,network -s /usr/bin/zsh krushn
+useradd -m -g users -G wheel -s /bin/bash juan
+# useradd -m -G wheel,power,iput,storage,uucp,network -s /usr/bin/zsh krushn
 sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 echo "Set password for new user krushn"
-passwd krushn
+passwd juan
 
 # Setup display manager
-systemctl enable sddm.service
+# systemctl enable sddm.service
 
 # Enable services
 systemctl enable NetworkManager.service
+
+# Enable ssh
+systemctl start sshd.service
 
 echo "Configuration done. You can now exit chroot."
