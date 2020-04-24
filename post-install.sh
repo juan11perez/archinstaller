fb1ce08a7e98936f9c45b6d9e7c9340f8319eea5
juan11perez <juan11perez@yahoo.com>
2020-04-24 17:05:06 +0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -36,6 +36,8 @@ passwd
 pacman -S openssh grub efibootmgr networkmanager pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server --noconfirm
 
 # Install bootloader
+mkdir /boot/efi
+mount /dev/vda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 # grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
