5aef1cfc2e5b8ff8e35cfafcb54fc566c7336319
juan11perez <juan11perez@yahoo.com>
2020-04-24 13:48:25 +0400

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/post-install.sh
+wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/install.sh
+chmod +x *.sh
+./install.sh
