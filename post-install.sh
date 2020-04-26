34a765a7bb363e97333cc135455ecff6dc1908a4
juan11perez <juan11perez@yahoo.com>
2020-04-26 18:10:57 +0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -72,9 +72,9 @@ hunspell-en_GB hunspell-es_co adobe-source-sans-pro-fonts  thunderbird user-mana
 xfce4-taskmanager gnome-disk-utility lightdm-gtk-greeter-settings xfce4-whiskermenu-plugin plank --noconfirm
 
 # Install icons - https://github.com/erikdubois/ArchXfce4
-wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/icons.sh
-chmod +x icons.sh
-./icons.sh
+git clone https://github.com/erikdubois/ArchXfce4.git
+cd ArchXcfe4/installation
+./300-install-themes-icons-cursors-conky-v1.sh
 
 echo "exec startxfce4" > ~/.xinitrc
 systemctl enable lightdm
