8a9652fd2711faad6c5757317805b25badbe5757
juan11perez <juan11perez@yahoo.com>
2020-04-27 19:40:09 +0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -72,6 +72,7 @@ hunspell-en_GB hunspell-es_co adobe-source-sans-pro-fonts  thunderbird user-mana
 xfce4-taskmanager gnome-disk-utility lightdm-gtk-greeter-settings xfce4-whiskermenu-plugin plank --noconfirm
 
 # Install icons - https://github.com/erikdubois/ArchXfce4
+cd /home/juan
 git clone https://github.com/erikdubois/ArchXfce4.git
 cd ArchXcfe4/installation
 ./300-install-themes-icons-cursors-conky-v1.sh
