06498da2b79b9753be92b791bc530fdb30790798
juan11perez <juan11perez@yahoo.com>
2020-04-30 20:43:09 +0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/post-install.sh
+wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/post-install-desktops.sh
 wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/install.sh
 # wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/300-install-themes-icons-cursors-conky-v1.sh
 
 echo "disk to format (sdx,vdx,nvme0n1)?"
 read which
-sed -i "s/disk=vda/disk=$which/g" /root/post-install.sh
+sed -i "s/disk=vda/disk=$which/g" /root/post-install-desktops.sh
 sed -i "s/disk=vda/disk=$which/g" /root/install.sh
 
 echo "enter new user name"
 read user
-sed -i "s/juan/$user/g" /root/post-install.sh
+sed -i "s/juan/$user/g" /root/post-install-desktops.sh
 
 chmod +x *.sh
 ./install.sh
