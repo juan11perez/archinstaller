505c84ec8100687ebb8d1e8b52d62dc0c743876f
juan11perez <juan11perez@yahoo.com>
2020-04-30 23:22:52 +0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/post-install.sh
 wget https://raw.githubusercontent.com/juan11perez/Arch-Installer/master/install.sh
 
-echo "disk to format (sdx,vdx,nvme0n1)?"
+echo "device to format (sdx,vdx,nvme0n1)?"
 read which
 sed -i "s/disk=vda/disk=$which/g" /root/post-install.sh
 sed -i "s/disk=vda/disk=$which/g" /root/install.sh
